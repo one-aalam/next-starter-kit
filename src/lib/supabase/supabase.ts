20ec29beb34a56254eccaccd7184435ca82d2656
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-03-25 20:32:15 +0530

    1.1 Integrate Supabase

---
+++ src/lib/supabase/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
