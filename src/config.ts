62c1527763f8f5c6a0d355102692c534c4b80a28
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-10-30 17:27:40 +0530

    feat: Enable auth middleware with a flag MW_AUTH

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
 export const ROUTE_HOME = '/profile'
 export const ROUTE_AUTH = '/'
 
+// control whether the auth middleware will be run or not
+export const MW_AUTH = false
 export const SUPA_TOKEN = 'sb:token'
--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,18 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server'
 import jwt from '@tsndr/cloudflare-worker-jwt'
-import { SUPA_TOKEN } from '~/config'
+import { MW_AUTH, SUPA_TOKEN } from '~/config'
 
 /**
  * Verifies the user's JWT token and continues with the chain of middlewares, till the actual route is visited if the user is valid, or redirects to `/` if it's not
  */
 export async function middleware(request: NextRequest) {
-  const token = request.cookies[SUPA_TOKEN]
+    if(MW_AUTH) await verifyAuth(request)
 
-  // Obtain JWT Secret from Supabase dashboard - 'Settings' -> 'API' -> 'Config' -> 'JWT Secret' and configure in Vercel as `SUPABASE_JWT_SECRET`
-  if (!token || !(await jwt.verify(token, process.env.SUPABASE_JWT_SECRET!))) {
-    return NextResponse.redirect('/', 302)
-  }
+    // continue the middleware chain https://nextjs.org/docs/api-reference/next/server#nextresponse
+    return NextResponse.next()
+}
 
-  // continue the middleware chain https://nextjs.org/docs/api-reference/next/server#nextresponse
-  return NextResponse.next()
+async function verifyAuth(request: NextRequest) {
+    const token = request.cookies[SUPA_TOKEN]
+
+    // Obtain JWT Secret from Supabase dashboard - 'Settings' -> 'API' -> 'Config' -> 'JWT Secret' and configure in Vercel as `SUPABASE_JWT_SECRET`
+    if (!token || !(await jwt.verify(token, process.env.SUPABASE_JWT_SECRET!))) {
+        return NextResponse.redirect('/', 302)
+    }
 }
